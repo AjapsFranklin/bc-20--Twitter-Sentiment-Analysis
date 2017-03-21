03872494a9569cbb434b7810a1d7625a7c452f5d
Franklin Ajaps <ajapsfranklin@gmail.com>
2017-03-21 19:35:21 +0100

    Adds function for fetching tweets

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 var readLineSync = require('readline-sync');
+var tweets = require('twit');
+var configFile = require('./twitter_config');
+console.log(configFile);
+
 
 //To get input from the command Line
 var userID = process.argv[2]; //Stores 2nd element passed in the command line
 if(typeof(userID)==="undefined"){
     userID = readLineSync.question("UserID:   ");//get UserID if it wasn't passed
 }
-    else{
-        //Call function to validate UserID
-    }
+
+
+//Call function to validate UserID
+if (checkUserID(userID) ===true)
+{
+    getTweets("@"+userID);
+}
+
 
 if(userID[0]="@")
-    userID=userID.subString(1,userID.length)
+    userID=userID.substring(1,userID.length)
 
 //TEST for edge cases
 AcceptedChar = "^[a-zA-Z0-9\s_]+$";
@@ -19,8 +28,21 @@ function checkUserID(userID){
         return "invalid input";
       else if(userID.length>14)
         return "UserID cannot be more than 15 characters";
-          else if(userID.match(AcceptedChar))
+          else if(!(userID.match("^[a-zA-Z0-9\s_]+$")))
             return "UserID should only be composed of A-Z, 0-9 and _(underscore)";
+              else
+                return true;
 }
 
+//GET Tweets
+function getTweets(userID){
+    var myTweets = new tweets(configFile)
+
+    myTweets.get('search/tweets', { q: 'banana since:2016-07-11', count: 100 }, function(err, data, response) {
+    console.log(response);
+})
+
+
+
+}
 
