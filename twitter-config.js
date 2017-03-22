e8a07928b73efac395f81a2bdd5f0064178f5443
Franklin Ajaps <ajapsfranklin@gmail.com>
2017-03-22 19:11:44 +0100

    Adds twitter config file

---
+++ twitter-config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  consumer_key:         'aE9EoGclg8PEMMKyy03RyIVKg',
+  consumer_secret:      'WZGJZueAzTKBy8JSaJm0a9UbpN9GW6z2v9jaCjZYD2jDc0jADT',
+  access_token:         '844204095804268545-29efzg8Tfd5Z5ARgZ34nZs47XNc2stJ',
+  access_token_secret:  '173iJUBMSAYt4oV7tKg9sWUZojWU0sSEB6QXISKsyu9ID',
+  //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
