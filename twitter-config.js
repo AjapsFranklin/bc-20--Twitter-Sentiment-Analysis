7377c81d5c206ee0dbf2364ba0f98b4aee5a5961
Franklin Ajaps <ajapsfranklin@gmail.com>
2017-03-24 00:01:26 +0100

    updates twitter configuration file

--- twitter-config.js
+++ twitter-config.js
@@ -1,8 +1,17 @@
 
-module.exports = {
+twitter_config = {
   consumer_key:         'aE9EoGclg8PEMMKyy03RyIVKg',
   consumer_secret:      'WZGJZueAzTKBy8JSaJm0a9UbpN9GW6z2v9jaCjZYD2jDc0jADT',
   access_token:         '844204095804268545-29efzg8Tfd5Z5ARgZ34nZs47XNc2stJ',
   access_token_secret:  '173iJUBMSAYt4oV7tKg9sWUZojWU0sSEB6QXISKsyu9ID',
   //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
+
+alchemyKey = {
+  api_key: 'accf81f1ef3420c935d3e3eddcf10882b55113d8'
+}
+
+module.exports = {
+  twitter_config:twitter_config,
+  alchemyKey:alchemyKey
 }
